fd9dc9ab4baf61ee3f3a8c46ed2004f943d545f8
Ailbhe Byrne <ailbhe.byrne287@mail.dcu.ie>
2021-02-28 16:58:13 +0000

    added test file

---
+++ src/test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+   printf("Executing...\n");
+   printf("Printing arg1:\n");
+   printf("%s\n", argv[1]);
+   printf("Finished!\n");
+   return 0;
+}
